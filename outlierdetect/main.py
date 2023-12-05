dabcb7c3ece6f341d8b371c133f5e816c37d137c
Anna <adixon@dimagi.com>
2023-12-05 20:33:14 +0000

    Working roud trip prototype

--- outlierdetect/main.py
+++ outlierdetect/main.py
@@ -1,23 +1,41 @@
 import outlierdetect
 import pandas as pd
-import json
+from sqlalchemy import create_engine
+import submit_data
+import yaml
 
-DATA_FILE = 'export.csv'
-QUESTIONS = ['form.received_va_dose_before']
+with open('config.yaml', 'r') as file:
+    config = yaml.safe_load(file)
+
+
+QUESTIONS = config['source_form_outlier_questions']
 aggregation_col = 'username'
+OUTLIER_RESULTS_CSV = 'outlier_results.csv'
 
-def flatten_outlier_output(output_dict, q):
-    flattened_data = {outer_k: inner_v[q] for outer_k, inner_v in output_dict.items() if q in inner_v}
-    print(flattened_data)
-
+def restructure_outlier_output(output_dict):
+    res = []
+    for username, user_outlier_results in output_dict.items():
+        for questionid, user_outlier_results in user_outlier_results.items():
+            case_name = username + '_' + questionid
+            user_question_results = {'username':username,'questionid': questionid, 'score':user_outlier_results['score'],
+                                     'p_value':user_outlier_results['p_value'],  'expected_freq':str(user_outlier_results['expected_freq']),
+                                     'observed_freq':str(user_outlier_results['observed_freq']), 'name':case_name}
+            res.append(user_question_results)
+    res_df = pd.DataFrame.from_records(res)
+    res_df.to_csv(OUTLIER_RESULTS_CSV,index=False)
 
 if __name__ == '__main__':
     #Load data
-    data = pd.read_csv(DATA_FILE)
-    # Compute MMA outlier scores
+    psql_connection = 'postgresql://postgres:postgres@postgres/postgres'
+    eng = create_engine(psql_connection)
+    sample_sql = "SELECT * FROM outlier_data_export;"
+    data = pd.read_sql(sample_sql, eng)
+    #Compute MMA outlier scores
     (mma_scores, _) = outlierdetect.run_mma(data, aggregation_col, QUESTIONS)
-    #flatten_outlier_output(mma_scores, QUESTIONS[0])
-    with open("mma_output.json", "w") as outfile: 
-        json.dump(mma_scores, outfile)
+    #Restructure the outlier scores for submission to CCHQ
+    restructure_outlier_output(mma_scores)
+    #Submit data to CCHQ
+    success, message = submit_data.main(OUTLIER_RESULTS_CSV)
+    
 
     
--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ "$#" -ne 5 ]; then
-    echo "run.sh <commcare hq> <commcare username> <commcare api key> <commcare auth mode> <commcare project>"
+if [ "$#" -ne 6 ]; then
+    echo "run.sh <commcare hq> <commcare username> <commcare api key> <commcare auth mode> <commcare project> <commcare password>"
     exit 1
 fi
 
-CC_HQ=$1 CC_USER=$2 CC_APIKEY=$3 CC_AUTH_MODE=$4 CC_PROJECT=$5 docker-compose up --build
+CC_HQ=$1 CC_USER=$2 CC_APIKEY=$3 CC_AUTH_MODE=$4 CC_PROJECT=$5 CC_PASSWORD=$6 docker-compose up --build
